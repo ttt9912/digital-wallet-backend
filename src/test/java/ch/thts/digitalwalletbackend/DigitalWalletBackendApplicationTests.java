3334eec88ab808b2197227304956a329c5ebfd0f
ttt <tschachtli.th@gmail.com>
2024-02-16 21:46:54 +0100

    project setup

---
+++ src/test/java/ch/thts/digitalwalletbackend/DigitalWalletBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.thts.digitalwalletbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalWalletBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
