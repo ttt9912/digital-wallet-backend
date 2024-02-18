6c973453c140d7b45fcae30433d0271d23946299
ttt <tschachtli.th@gmail.com>
2024-02-18 22:41:55 +0100

    add cors config

--- digital-wallet-backend-rest-api/src/main/java/ch/thts/digitalwalletbackend/restapi/account/AccountsController.java
+++ digital-wallet-backend-rest-api/src/main/java/ch/thts/digitalwalletbackend/restapi/account/AccountsController.java
@@ -3,12 +3,14 @@ package ch.thts.digitalwalletbackend.restapi.account;
 import ch.thts.digitalwalletbackend.business.accounts.Account;
 import ch.thts.digitalwalletbackend.business.accounts.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/accounts")
 @RequiredArgsConstructor
